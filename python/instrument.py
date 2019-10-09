470de897bec14d13e7de324bd5cf663f56978f6d
jmmelko <jmmelko@gmail.com>
2019-10-09 12:00:15 +0200

    Add files via upload

--- python/instrument.py
+++ python/instrument.py
@@ -16,7 +16,11 @@ class Harp:
         self.instrument_type = 'harp'
         self.is_highlighted = False
 
-
+        self.sky_inverse_position_map = {
+                (0, 0): 'A1', (0, 1): 'A2', (0, 2): 'A3', (0, 3): 'A4', (0, 4): 'A5',
+                (1, 0): 'B1', (1, 1): 'B2', (1, 2): 'B3', (1, 3): 'B4', (1, 4): 'B5',
+                (2, 0): 'C1', (2, 1): 'C2', (2, 2): 'C3', (2, 3): 'C4', (2, 4): 'C5'
+                }
 
     def get_row_count(self):
         return self.row_count
@@ -37,6 +41,7 @@ class Harp:
         '''
         The chord_image is a dictionary. The keys are tuples representing the positions of the buttons. The values are dictionaries, where each key is the frame, and the value is a Boolean indicating whether the button is highlighted in that frame.
         '''
+        # Ok, but in this case the dict should have keys for all the positions, and shut down buttons should be set to False
         #TODO: Raise TypeError if chord_image is not a dict
         self.chord_image = chord_image
 
@@ -53,7 +58,7 @@ class Harp:
         highlighted_states = row[column_index]
         highlighted_states.append(new_state)
 
-        chord_image[index] = highlighted_states
+        chord_image[index] = highlighted_states #index is undefined
 
         self.set_chord_image(chord_image)
 
@@ -61,7 +66,17 @@ class Harp:
     def get_chord_image(self):
         return self.chord_image
 
-
+    def ascii_from_chord_image(self, chord_image, instrument_index):
+        
+        ascii_chord = ''
+        for k in chord_image:
+            for f in chord_image[k]:
+                if chord_image[k][f]==True: # Button is highlighted
+                    ascii_chord += self.sky_inverse_position_map[k]
+                     #print(str(k) + ' = ' + ascii_chord)
+        return ascii_chord
+        
+        
 
     def render_from_chord_image(self, chord_image, note_width, instrument_index):
 
--- python/modes.py
+++ python/modes.py
@@ -2,9 +2,13 @@ from enum import Enum
 
 class InputMode(Enum):
     KEYBOARD = 1
-    ALPHANUMERIC = 2
+    SKY = 2
+    WESTERN = 3
+    JIANPU = 4
+    SKYFILE = 5
+    WESTERNFILE = 6
+    JIANPUFILE = 7
 
-
-class RenderMode(Enum):
+class RenderMode(Enum): # Currently unused as of october 9th 2019
     VISUAL = 1
-    ALPHANUMERIC = 2
+    ASCII = 2
