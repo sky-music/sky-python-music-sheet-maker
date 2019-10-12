960414f9b2a76b93c44e193e9edcaf497ed41807
Tracey <traceyle@outlook.com>
2019-10-12 17:22:35 +1100

    Bug: Set a different quaver delimiter for Jianpu

--- python/main.py
+++ python/main.py
@@ -13,13 +13,14 @@ import re
 class Parser:
 
     def __init__(self):
-        
+
         if (os.getenv('LANG') == 'fr') or (os.getenv('LANG') == 'be'):
             self.keyboard_position_map = {'A': (0, 0), 'Z': (0, 1), 'E': (0, 2), 'R': (0, 3), 'T': (0, 4), 'Q': (1, 0), 'S': (1, 1), 'D': (1, 2), 'F': (1, 3), 'G': (1, 4), 'W': (2, 0), 'X': (2, 1), 'C': (2, 2), 'V': (2, 3), 'B': (2, 4)}
             self.keyboard_layout="AZERT QSDFG WXCVB"
         else:
             self.keyboard_position_map = {'Q': (0, 0), 'W': (0, 1), 'E': (0, 2), 'R': (0, 3), 'T': (0, 4), 'A': (1, 0), 'S': (1, 1), 'D': (1, 2), 'F': (1, 3), 'G': (1, 4), 'Z': (2, 0), 'X': (2, 1), 'C': (2, 2), 'V': (2, 3), 'B': (2, 4)}
             self.keyboard_layout="QWERT ASDFG ZXCVB"
+
         self.sky_position_map = {
                 'A1': (0, 0), 'A2': (0, 1), 'A3': (0, 2), 'A4': (0, 3), 'A5': (0, 4),
                 'B1': (1, 0), 'B2': (1, 1), 'B3': (1, 2), 'B4': (1, 3), 'B5': (1, 4),
@@ -56,24 +57,25 @@ class Parser:
 
     def get_western_position_map(self):
         return self.western_position_map
-    
+
     def get_jianpu_position_map(self):
         return self.jianpu_position_map
-    
-    def parse_icon(self, icon, delimiter, input_mode):
-        tokens = icon.split(delimiter)
+
+    def parse_icon(self, icon, QUAVER_DELIMITER, input_mode):
+
+        tokens = icon.split(QUAVER_DELIMITER)
         return tokens
-    
-    def parse_line(self, line, icon_delimiter, blank_icon, chord_delimiter, input_mode, octave_shift=0):
+
+    def parse_line(self, line, icon_delimiter, blank_icon, QUAVER_DELIMITER, input_mode, octave_shift=0):
         '''
         Returns instrument_line: a list of chord images
-        ''' 
+        '''
         icons = line.rstrip().lstrip().replace(icon_delimiter+icon_delimiter,icon_delimiter).split(icon_delimiter)
         instrument_line = []
 
         #TODO: Implement logic for parsing line vs single icon.
         for icon in icons:
-            chords = self.parse_icon(icon, chord_delimiter, input_mode)
+            chords = self.parse_icon(icon, QUAVER_DELIMITER, input_mode)
             results = self.parse_chords(chords, blank_icon, input_mode, octave_shift)
             chord_image = results[0]
             harp_is_highlighted = results[1]
@@ -101,8 +103,8 @@ class Parser:
         else:
             position_map = self.get_keyboard_position_map()
         note = note.upper()
-               
-        if note in position_map.keys():           
+
+        if note in position_map.keys():
             pos=position_map[note] #tuple
             idx=pos[0]*5+pos[1]
             idxshift=idx+7*octave_shift
@@ -122,7 +124,7 @@ class Parser:
             raise KeyError
 
     def parse_chords(self, chords, blank_icon, input_mode, octave_shift=0):
-        
+
         is_empty = True
         chord_image = {}
         for chord_idx, chord in enumerate(chords):
@@ -132,8 +134,8 @@ class Parser:
                 chord=wrap(chord, 2) # Notes are always 2 character-long in Sky and Western notations
 
             if input_mode in [InputMode.JIANPU, InputMode.JIANPUFILE]:
-                chord=re.sub('([1-9])',' \\1',chord).split()             
-            
+                chord=re.sub('([1-9])',' \\1',chord).split()
+
             for note in chord: # Chord is a list of notes
                 #Except InvalidLetterException
                 try:
@@ -150,7 +152,13 @@ class Parser:
 
 
 # Dash separated list of chords
-CHORD_DELIMITER = '-'
+
+QUAVER_DELIMITER = '-'
+JIANPU_QUAVER_DELIMITER = '^'
+
+if input_mode == InputMode.JIANPU or input_mode == InputMode.JIANPUFILE:
+    QUAVER_DELIMITER = JIANPU_QUAVER_DELIMITER
+
 ICON_DELIMITER = ' '
 NOTE_WIDTH = "1em"
 BLANK_ICON = '.'
@@ -162,7 +170,7 @@ class Error(Exception):
     """Base class for exceptions in this module."""
     pass
 
-class BlackIconError(Error):
+class BlankIconError(Error):
     pass
 
 ### Change directory
@@ -183,14 +191,14 @@ song_dir=os.getcwd()
 #print("3) Import sheet from a file using Sky notation.")
 #print("4) Type directly using *musical* notation ().")
 #print("5) Import sheet from a file using *musical* notation.")
- 
+
 print('===== NEW SONG =====')
 print("Choose input mode:\n")
 print("1) Use " + Parser().keyboard_layout + " as keyboard.")
 print("2) Type in the command line.")
 print("3) Import a text file.")
 
-   
+
 try:
     song_input_mode = int(input("Mode (1-3): ").strip())
 except ValueError:
@@ -204,10 +212,10 @@ if song_input_mode != 1:
     try:
         song_notation = int(input("Mode (1-3): ").strip())
     except ValueError:
-        song_notation = 1    
+        song_notation = 1
 else:
     song_notation = 1
-    
+
 if song_input_mode != 1 and song_notation != 1:
     try:
         octave_shift = int(input('Octave shift (-3 -2 -1 0 1 2 3): ').strip())
@@ -215,7 +223,7 @@ if song_input_mode != 1 and song_notation != 1:
         octave_shift = 0
 else:
     octave_shift = 0
-    
+
 if song_input_mode == 1:
     song_input_mode = InputMode.KEYBOARD
 elif song_input_mode == 2 and song_notation == 1:
@@ -235,7 +243,7 @@ else:
 
 print('\nSeparate blocks of notes with \"' + ICON_DELIMITER + '\".')
 print('Use \"' + BLANK_ICON + '\" for a blank block.')
-print('If you want multiple colours within an icon, separate the colours with \"' + CHORD_DELIMITER + '\".')
+print('If you want multiple colours within an icon, separate the colours with \"' + QUAVER_DELIMITER + '\".')
 print('=========================')
 
 instrument_lines = [] # A list of instrument_lines
@@ -246,21 +254,21 @@ if song_input_mode in [InputMode.SKYFILE, InputMode.WESTERNFILE, InputMode.JIANP
         fic = open(fp,mode='r')
         print(['Opening file: ' + os.path.abspath(fp)])
         for song_line in fic:
-            instrument_line = parser.parse_line(song_line.rstrip(), ICON_DELIMITER, BLANK_ICON, CHORD_DELIMITER, song_input_mode, octave_shift)            
+            instrument_line = parser.parse_line(song_line.rstrip(), ICON_DELIMITER, BLANK_ICON, QUAVER_DELIMITER, song_input_mode, octave_shift)
             instrument_lines.append(instrument_line)
-            
+
         fic.close()
     except (OSError, IOError) as err:
          print('Error opening file.')
          raise err
 else:
     song_line = input('Type line: ')
-    while song_line: 
-        instrument_line = parser.parse_line(song_line, ICON_DELIMITER, BLANK_ICON, CHORD_DELIMITER, song_input_mode, octave_shift)    
-        instrument_lines.append(instrument_line)   
+    while song_line:
+        instrument_line = parser.parse_line(song_line, ICON_DELIMITER, BLANK_ICON, QUAVER_DELIMITER, song_input_mode, octave_shift)
+        instrument_lines.append(instrument_line)
         song_line = input('Type line: ')
 
-    
+
 print('============')
 print('Press ENTER to skip the following.')
 musical_key = input('Recommended musical key: ')
@@ -309,6 +317,6 @@ if song_input_mode in [InputMode.WESTERN, InputMode.JIANPU, InputMode.WESTERNFIL
     with open(ascii_file_path, 'w+') as ascii_file:
         ascii_file.write(render_instrument_lines(instrument_lines, NOTE_WIDTH, RenderMode.ASCII))
     ascii_full_file_path = os.path.join(str(song_dir), ascii_file_path)
-    print('Your converted song is located at', ascii_full_file_path)    
-        
+    print('Your converted song is located at', ascii_full_file_path)
+
 os.chdir(mycwd)
