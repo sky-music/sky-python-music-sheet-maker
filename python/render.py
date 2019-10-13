08cb8fcafd1880b8519404f71603ff669f1f82b1
jmmelko <jmmelko@gmail.com>
2019-10-13 15:07:24 +0200

    Changes to accomodate for new Render Modes

--- python/render.py
+++ python/render.py
@@ -9,9 +9,9 @@ def render_instrument_line(instrument_line, instrument_index, note_width, render
             instrument_chord_image = instrument.get_chord_image() # A dict with position tuple and values Frame:True/False
             instrument_render = instrument.render_from_chord_image(instrument_chord_image, note_width, instrument_index) 
             instrument_render += '\n\n'
-        elif render_mode == RenderMode.ASCII:
+        elif render_mode in [RenderMode.SKYASCII, RenderMode.WESTERNASCII, RenderMode.JIANPUASCII]:
             instrument_chord_image = instrument.get_chord_image() # A dict with position tuple and values Frame:True/False
-            instrument_render = instrument.ascii_from_chord_image(instrument_chord_image) 
+            instrument_render = instrument.ascii_from_chord_image(instrument_chord_image, render_mode) 
             instrument_render += ' '
         instrument_index += 1
         line_render += instrument_render
@@ -30,7 +30,7 @@ def render_instrument_lines(instrument_lines, note_width, render_mode):
         if len(line_render)>0:
             if render_mode == RenderMode.VISUAL:
                 song_render += '\n<br />\n<hr />\n'
-            elif render_mode == RenderMode.ASCII:
+            elif render_mode in [RenderMode.SKYASCII, RenderMode.WESTERNASCII, RenderMode.JIANPUASCII]:
                     song_render += '\n'   
 
     return song_render
