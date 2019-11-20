34201f0a669408ad9a3549931ac0e9e158203e9d
jmmelko <jmmelko@gmail.com>
2019-11-20 14:28:08 +0100

    Cosmetic bug fix
    Deleted a newline break before repeat table that cause a blank to be rendered in the html

--- python/instruments.py
+++ python/instruments.py
@@ -276,7 +276,6 @@ class Harp(Instrument):
                 harp_render += '<td>'
 
                 note = self.get_note_from_position((row, col))
-                #note.set_position(row, col)
 
                 note_render = note.render_in_html(note_width)
                 harp_render += note_render
@@ -287,7 +286,7 @@ class Harp(Instrument):
         harp_render += '</table>'
         
         if self.get_repeat() > 0:
-            harp_render += '\n<table class=\"harp-' + str(self.get_index()) + ' repeat\">'
+            harp_render += '<table class=\"harp-' + str(self.get_index()) + ' repeat\">'
             harp_render += '<tr>'
             harp_render += '<td>'
             harp_render += 'x' + str(self.get_repeat())       
