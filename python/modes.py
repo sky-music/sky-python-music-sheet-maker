4b1bb7588ff2a7e62205880259ab4170495fd3c7
Tracey <traceyle@outlook.com>
2019-11-23 11:23:44 +1100

    Small typo

--- python/modes.py
+++ python/modes.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 class InputModes(Enum):
     SKYKEYBOARD = [1, 'Sky keyboard', 'Type on keyboard as you would in Sky']
-    SKY = [2, 'Sky ABC1-5', 'Sky colum/row notation:\n   A1 A2 A3 A4 A5\n   B1 B2 B3 B4 B5\n   C1 C2 C3 C4 C5']
+    SKY = [2, 'Sky ABC1-5', 'Sky column/row notation:\n   A1 A2 A3 A4 A5\n   B1 B2 B3 B4 B5\n   C1 C2 C3 C4 C5']
     WESTERN = [3, 'Western CDEFGAB2-7', 'Western (note name + octave, e.g. C4 D4 E4 ..., or do4 re4 mi4 ...)']
     JIANPU = [4, 'Jianpu 1234567+-', 'Jianpu (note names as 1 2 3 4 5 6 7, followed by + or - for octaves)']
     WESTERNCHORDS = [5, 'Chords CDEFGAB', 'Abbrev. chord name (C, F, Dm, Bdim, A+, Csus2, Dsus4, C6, Cmaj7, Dm11)']
@@ -14,7 +14,7 @@ class RenderModes(Enum):
     SKYASCII = 4
     WESTERNASCII = 5
     JIANPUASCII = 6
-    
+
 class CSSModes(Enum):
     XML = 1
     HREF = XML
