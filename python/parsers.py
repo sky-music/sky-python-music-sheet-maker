5200b40e47f048a5439a9258ac780b94595e2f48
jmmelko <jmmelko@gmail.com>
2019-12-06 19:30:49 +0100

    Bug fix in regex of SkyKeyboard
    When concatening a string the regex identifier ’r’ be repeated before each string segment

--- python/parsers.py
+++ python/parsers.py
@@ -770,11 +770,12 @@ class SkyKeyboardNoteParser(SkyNoteParser):
             self.position_map = {'.': (-1, -1), 'Q': (0, 0), 'W': (0, 1), 'E': (0, 2), 'R': (0, 3), 'T': (0, 4), 'A': (1, 0), 'S': (1, 1), 'D': (1, 2), 'F': (1, 3), 'G': (1, 4), 'Z': (2, 0), 'X': (2, 1), 'C': (2, 2), 'V': (2, 3), 'B': (2, 4)}
             self.keyboard_layout="QWERT ASDFG ZXCVB"
 
-        regex = self.keyboard_layout.replace(' ','') + self.keyboard_layout.replace(' ','').lower()
-        self.note_name_with_octave_regex = re.compile(r'(['+regex+'])')
+        regex = self.keyboard_layout.replace(' ','')
+        regex += regex.lower()
+        self.note_name_with_octave_regex = re.compile(r'(['+regex+r'])')
         self.note_name_regex = self.note_name_with_octave_regex
-        self.single_note_name_regex = re.compile(r'(\b['+regex+']\b)')
-        self.not_note_name_regex = re.compile(r'[^'+regex+']+')
+        self.single_note_name_regex = re.compile(r'(\b['+regex+r']\b)')
+        self.not_note_name_regex = re.compile(r'[^'+regex+r']+')
         self.not_octave_regex = re.compile(r'\\.')
 
 
