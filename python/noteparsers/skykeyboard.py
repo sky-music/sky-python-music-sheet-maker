62f9ff384f26eb7da3ea3602b82b803d687f18ef
jmmelko <jmmelko@gmail.com>
2020-02-07 08:13:09 +0100

    fix for #106

--- python/noteparsers/skykeyboard.py
+++ python/noteparsers/skykeyboard.py
@@ -14,14 +14,13 @@ class SkyKeyboard(sky.Sky):
             self.position_map = {'.': (-1, -1), 'A': (0, 0), 'Z': (0, 1), 'E': (0, 2), 'R': (0, 3), 'T': (0, 4),
                                  'Q': (1, 0), 'S': (1, 1), 'D': (1, 2), 'F': (1, 3), 'G': (1, 4), 'W': (2, 0),
                                  'X': (2, 1), 'C': (2, 2), 'V': (2, 3), 'B': (2, 4)}
-            self.keyboard_layout = "AZERT QSDFG WXCVB"
+            regex = "AZERTQSDFGWXCVB"
         else:
             self.position_map = {'.': (-1, -1), 'Q': (0, 0), 'W': (0, 1), 'E': (0, 2), 'R': (0, 3), 'T': (0, 4),
                                  'A': (1, 0), 'S': (1, 1), 'D': (1, 2), 'F': (1, 3), 'G': (1, 4), 'Z': (2, 0),
                                  'X': (2, 1), 'C': (2, 2), 'V': (2, 3), 'B': (2, 4)}
-            self.keyboard_layout = "QWERT ASDFG ZXCVB"
+            regex = "QWERTASDFGZXCVB"
 
-        regex = self.keyboard_layout.replace(' ', '')
         regex += regex.lower()
         self.note_name_with_octave_regex = re.compile(r'([' + regex + r'])')
         self.note_name_regex = self.note_name_with_octave_regex
