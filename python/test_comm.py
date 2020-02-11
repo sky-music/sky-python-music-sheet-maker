9c78a22a43562917b2c51dd4dc5c08d1f984a7f1
jmmelko <jmmelko@gmail.com>
2020-02-11 21:56:40 +0100

    a script to test communicate.py

---
+++ python/test_comm.py
@@ -0,0 +1,24 @@
+import os
+from modes import InputMode
+from communication import QueryOpen, QueryChoice, Query
+    
+class test_responder():
+	
+    def __init__(self):
+    	
+        self.song_dir_in = 'test_songs'
+        self.song_dir_out = 'songs_out'
+        self.name = 'music-cog'
+        
+    def create_query_mode(self, modes_list):
+
+        query_mode = QueryChoice(sender=self.name, recipient='bot', question="Mode (1-" + str(len(modes_list)) + "): ",
+                                 foreword="Please choose your note format:\n", afterword="None", choices=modes_list)
+
+        return query_mode            
+            
+resp = test_responder()
+q = resp.create_query_mode(modes_list=[InputMode.JIANPU])
+q.send()
+
+
