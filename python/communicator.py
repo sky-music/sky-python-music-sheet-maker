a47c8ffd939e2bd9143e694f731be4ff036d5fa8
Tracey <traceyle@outlook.com>
2020-02-17 20:32:20 +1100

    outline some more queries for #109

--- python/communicator.py
+++ python/communicator.py
@@ -39,26 +39,38 @@ class Communicator:
         # TODO:
 
         # query notes
-        # query song notation / input modes
-        # query song key
-        # query note shift
-        # info error ratio
-        # query song title
-        # query song headers
-        # send song
-
-        q_song_title = QueryOpen(sender='music-cog', recipient='bot', question='What is the song title? (also used '
-                                                                               'for the file name)', foreword='',
-                                 afterword=None,
-                                 reply_type=ReplyType.TEXT, limits=None)
-        self.brain.store(q_song_title)
-
+        # query song notation / input mode
         modes_list = [InputMode.JIANPU, InputMode.SKY]
         q_mode = QueryChoice(sender='music-cog', recipient='bot', question="Mode (1-" + str(len(modes_list)) + "): ",
                              foreword="Please choose your note format:\n", afterword=None,
                              reply_type=ReplyType.INPUTMODE,
                              limits=modes_list)
         self.brain.store(q_mode)
+
+        # query song key
+
+        # query note shift
+
+        # info error ratio
+
+        # query song title
+        q_song_title = QueryOpen(sender='music-cog', recipient='bot', question='What is the song title? (also used '
+                                                                               'for the file name)', foreword='',
+                                 afterword=None,
+                                 reply_type=ReplyType.TEXT, limits=None)
+        self.brain.store(q_song_title)
+
+        # query original_artists
+        q_original_artists = QueryOpen(sender='music-cog', recipient='bot', question='Original artist(s): ',
+                                       foreword='Please fill song info or press ENTER to skip:', afterword=None,
+                                       reply_type=ReplyType.TEXT, limits=None)
+        self.brain.store(q_original_artists)
+        # query transcript_writer
+
+        # send song
+
+        # query.send()
+        # query.receive('example answer')
 
     def recall_queries(self):
 
