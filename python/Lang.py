b70e4ed6267d84f593cbebb9d369c1a59281afe9
jmmelko <jmmelko@gmail.com>
2020-04-09 21:57:54 +0200

    #110 Serious attempt at localization
    Borrowed (ok, stolen) code from Alex.
    I just have to decide how to get the locale from the user.
    On the command line, with os get env, but then where to store it???
    Most likely in music maker
    On the website, have WebsitePlayer get the info and then pass it to
    music maker ?
    In the bot cog pass the information to music_maker.

--- python/Lang.py
+++ python/Lang.py
@@ -1,15 +1,21 @@
 import yaml
 
 LANG = dict()
-loaded = False
-
+locales = ['en_US', 'ja_JP', 'fr_FR']
+loaded = dict((locale, False) for locale in locales)
+warn_count = 0
+'''
+def get_by_path(root, items):
+    """Access a nested object in root by item sequence."""
+    return reduce(operator.getitem, items, root)
+'''
+'''
 def load():
     global LANG, loaded
     with open("lang.yaml") as file:
         LANG = yaml.safe_load(file)
     loaded = True
-
-
+    
 def get_string(key, **kwargs):
     if not loaded:
         load()
@@ -25,4 +31,42 @@ def get_string(key, **kwargs):
         if isinstance(obj[i], str):
             return obj[i].format(**kwargs)
         elif isinstance(obj[i], dict):
-            obj = obj[i] #Recursive search of yaml nested dictionaries???
+            obj = obj[i]
+'''
+
+def load(locale):
+    global LANG, loaded
+    with open("langs/%s.yaml"%locale) as file:
+        LANG[locale] = yaml.safe_load(file)
+    loaded[locale] = True
+
+
+def get_string(key, locale=None, **kwargs):
+    global locales, warn_count
+    if locale not in locales:
+        if not loaded[locales[0]]:
+            print('\n***WARNING: bad locale %s for key %s. Reverting to %s\n'%(locale, str(key), locales[0]))
+        locale = locales[0]
+    
+    if not loaded[locale]:
+        load(locale)
+
+    key_list = key.split("/")
+    obj = LANG[locale]
+    for i in key_list:
+        if i not in obj:
+            if warn_count < 10:
+                print('\n***WARNING: could not find lang key %s for locale %s\n'%(str(i), locale))
+                warn_count += 1
+            return ''
+            #raise KeyError(f"Unknown lang key: {i}")
+
+        if isinstance(obj[i], str):
+            return obj[i].format(**kwargs)
+        elif isinstance(obj[i], dict):
+            obj = obj[i]
+
+            
+            
+            
+            
