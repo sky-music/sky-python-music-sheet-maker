d8be9bdd5feef918d97eb201e7975ee7897b4aad
jmmelko <jmmelko@gmail.com>
2020-05-12 18:56:25 +0200

    Hotfix for #152
    The r prefix meaning that a backslash must not be doubled to indicate an escape sentence.

--- python/noteparsers/jianpu.py
+++ python/noteparsers/jianpu.py
@@ -13,12 +13,12 @@ class Jianpu(noteparser.NoteParser):
                                      '5b': 6, '5': 7, '5#': 8, '6b': 8, '6': 9, '6#': 10, '7b': 10, '7': 11}
 
         # Compile regexes for notes to save before using
-        self.note_name_with_octave_regex = re.compile(r'([1234567][b#]?[\\+\\-]+)')
+        self.note_name_with_octave_regex = re.compile(r'([1234567][b#]?[\+\-]+)')
         self.note_name_regex = re.compile(r'([1234567][b#]?)')
-        self.single_note_name_regex = re.compile(r'\b[1234567][b#]?[\\+\\-]?\b')
-        self.note_octave_regex = re.compile(r'[\\+\\-]+')
+        self.single_note_name_regex = re.compile(r'\b[1234567][b#]?[\+\-]?\b')
+        self.note_octave_regex = re.compile(r'[\+\-]+')
         self.not_note_name_regex = re.compile(r'[^1234567b#]+')
-        self.not_octave_regex = re.compile(r'[^\\+\\-]+')
+        self.not_octave_regex = re.compile(r'[^\+\-]+')
 
         self.inverse_position_map = {
             (0, 0): '1', (0, 1): '2', (0, 2): '3', (0, 3): '4', (0, 4): '5',
@@ -28,7 +28,7 @@ class Jianpu(noteparser.NoteParser):
 
     def get_note_octave(self, note):
 
-        note_octave = re.search('(\\+)+', note)
+        note_octave = re.search(r'(\+)+', note)
         if note_octave is not None:
             note_octave = self.get_default_starting_octave() + len(note_octave.group(0))
             return note_octave
