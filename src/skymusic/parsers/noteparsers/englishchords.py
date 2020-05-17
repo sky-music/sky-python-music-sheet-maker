127e0fa5f17e2ecf4fa96d37e002e6082871fdf1
jmmelko <jmmelko@gmail.com>
2020-05-17 17:06:52 +0200

    typo

--- src/skymusic/parsers/noteparsers/englishchords.py
+++ src/skymusic/parsers/noteparsers/englishchords.py
@@ -23,7 +23,7 @@ class EnglishChords(english.English):
             'Esus{x}': f"E{x}A{x}B{x}", 'Gsus{x}': f"G{x}C{y}D{y}", 'Asus{x}': f"A{x}D{y}E{y}",
             'D7sus{x}': f"D{x}G{x}A{x}C{y}", 'E7sus{x}': f"E{x}A{x}B{x}D{y}",
             'G7sus{x}': f"G{x}C{y}D{y}F{y}", 'A7sus{x}': f"A{x}D{y}E{y}G{y}",
-            'C6': f"C{x}E{x}G{x}A{x}", 'F6': f"F{x}A{x}C{y}D{y}", 'G6': f"G{x}B{x}D{y}E{y}",
+            'C6': f"C{x}E{x}G{x}A{x}", 'F6': f"F{x}A{x}C{y}D{y}", 'Fmaj6': f"F{x}A{x}C{y}D{y}", 'G6': f"G{x}B{x}D{y}E{y}",
             'G7': f"G{x}B{x}D{y}F{y}", 'G9': f"G{x}B{x}D{y}F{y}A{y}",
             'Cmaj7': f"C{x}E{x}G{x}B{x}", 'Fmaj7': f"F{x}A{x}C{y}E{y}",
             'Cmaj9': f"C{x}E{x}G{x}B{x}D{y}", 'Fmaj9': f"F{x}A{x}C{y}E{y}G{y}",
