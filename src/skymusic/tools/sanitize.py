2d1cda8ee089e7e31b3c855e697dccccdbff6e2c
jmmelko <jmmelko@gmail.com>
2020-09-01 17:38:50 +0200

    simplified tool

--- src/skymusic/tools/sanitize.py
+++ src/skymusic/tools/sanitize.py
@@ -20,11 +20,9 @@ with open(filepath, 'r', encoding='utf-8', errors='ignore') as fp:
 song = re.sub(',',' . ', song)
 song = re.sub('(\s){2,}',r'\1', song)
 chord_matches = re.finditer('(\[(?:(?:\w+\s*)+)\])', song)
-for mat in chord_matches:
-    notes_match = re.search('\[((?:\w|\s)+)\]',mat.group(0))
-    notes = notes_match.group(1) if not None else None
-    if notes:
-        song = re.sub(notes, re.sub('\s','',notes), song)
+for match in chord_matches:
+    notes = re.sub('\s|\[|\]|\(|\)','',match.group(0))
+    song = song.replace(match.group(0), notes)
 
 song = song.replace('[','')
 song = song.replace(']','')
@@ -32,7 +30,7 @@ song = song.strip()
 
 (base, ext) = os.path.splitext(filename)
 new_filename = base + '_2' + ext
-new_filepath = os.path.join(songs_dir, filename)
+new_filepath = os.path.join(songs_dir, new_filename)
 
 with open(new_filepath, 'w', encoding='utf-8') as fp2:
     
