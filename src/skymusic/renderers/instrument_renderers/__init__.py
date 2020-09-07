61b57e42cfac0d71b9d070ab608f80e98d0d3161
jmmelko <jmmelko@gmail.com>
2020-09-07 08:36:28 +0200

    cosmetics and install scripts

--- src/skymusic/renderers/instrument_renderers/__init__.py
+++ src/skymusic/renderers/instrument_renderers/__init__.py
@@ -1,8 +1,7 @@
 from . import instrument_renderer, html_ir, svg_ir, png_ir, midi_ir, skyjson_ir, ascii_ir
 __all__ = [instrument_renderer, html_ir, svg_ir, png_ir, midi_ir, skyjson_ir, ascii_ir]
 
-import os
-import sys
+import os, sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
